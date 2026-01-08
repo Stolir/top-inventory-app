38c9d72b6ae913b272397fc62b9bdb5be58581ba
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-01-08 17:19:28 +0300

    Add functions to get featured games, award winners, and games by ID

--- models/gamesModel.js
+++ models/gamesModel.js
@@ -13,4 +13,42 @@ async function getAllGames() {
   }
 }
 
-module.exports = { getAllGames };
+async function getFeaturedGames() {
+  const query =
+    "SELECT games.*, genres.name AS genre FROM games JOIN genres ON games.genre_id=genres.id WHERE games.id IN (SELECT * FROM featured_games) ORDER BY games.id ASC";
+
+  try {
+    const { rows } = await pool.query(query);
+    return rows;
+  } catch (err) {
+    console.error("Error getting featured games: ", err);
+    throw err;
+  }
+}
+
+async function getGameAwards() {
+  const query =
+    "SELECT games.*, genres.name AS genre FROM games JOIN genres ON games.genre_id=genres.id WHERE games.id IN (SELECT game_id FROM award_winners) ORDER BY games.id ASC";
+
+  try {
+    const { rows } = await pool.query(query);
+    return rows;
+  } catch (err) {
+    console.error("Error getting game award winners: ", err);
+    throw err;
+  }
+}
+
+async function getGameById(id) {
+  const query =
+    "SELECT games.*, genre.name FROM games JOIN genres ON games.genre_id=genres.id WHERE id = $1";
+  try {
+    const { rows } = await pool.query(query, [id]);
+    return rows;
+  } catch (err) {
+    console.error(`Error getting game with id: ${id}`, err);
+    throw err;
+  }
+}
+
+module.exports = { getAllGames, getFeaturedGames, getGameAwards, getGameById };
