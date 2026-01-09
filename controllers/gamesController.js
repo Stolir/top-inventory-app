2a4194d951e246b8304f9e793ef90a43a271b8f0
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-01-09 16:27:19 +0300

    Add all games page

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,21 +1,22 @@
 const { getDeveloperByGameId } = require("../models/gameDevelopersModel");
-const { getGameById } = require("../models/gamesModel");
+const { getGameById, getAllGames } = require("../models/gamesModel");
 const { getTagsByGameId } = require("../models/gameTagsModel");
 const { getNavLinks } = require("../models/navbarModel");
 
 const links = getNavLinks();
 
 async function getAllGamesPage(req, res) {
-  res.render("placeholder", { title: "Games", links });
+  const games = await getAllGames();
+  res.render("allGamesPage", { title: "All Games", games, links });
 }
 
 async function getGamePage(req, res) {
   const { gameId } = req.params;
-  console.log(`getting page with id ${gameId}`);
+
   const game = await getGameById(gameId);
   const tags = await getTagsByGameId(gameId);
   const developers = await getDeveloperByGameId(gameId);
-  console.log(game, tags, developers);
+  console.log(developers);
   res.render("gamePage", { game, tags, developers, links });
 }
 
