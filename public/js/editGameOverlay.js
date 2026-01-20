42d9efd28a8165206164f4300cdd5662de01c9bb
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-01-20 17:38:30 +0300

    Add editing overlay and buttons to edit and delete games to game page

---
+++ public/js/editGameOverlay.js
@@ -0,0 +1,39 @@
+const editGameOverlayBackdrop = document.querySelector("#edit-backdrop");
+const editGameOverlay = document.querySelector(".edit-game-overlay");
+const updateBtn = document.getElementById("updateGame");
+const cancelBtn = document.querySelector("#cancel-edit");
+
+updateBtn.addEventListener("click", () => {
+  openEdit();
+});
+
+cancelBtn.addEventListener("click", () => {
+  closeEdit();
+});
+
+function openEdit() {
+  page.inert = true;
+  editGameOverlayBackdrop.hidden = false;
+  page.classList.toggle("blur");
+}
+
+function closeEdit() {
+  page.inert = false;
+  editGameOverlayBackdrop.hidden = true;
+  page.classList.toggle("blur");
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && editGameOverlayBackdrop.hidden === false) {
+    closeEdit();
+  }
+});
+
+editGameOverlayBackdrop.addEventListener("click", (e) => {
+  if (!editGameOverlay.contains(e.target)) {
+    closeEdit();
+  }
+});
+
+// Choices library multiple select config
+const tagsSelect = new Choices("#tags-select", { removeItemButton: true });
